69735dbf501b4691052a5b6a0ede7980b8ce2feb
24c02 <163450896+24c02@users.noreply.github.com>
2025-08-26 18:31:16 -0400

    secret key base dummy? no, i'm the dummy

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,7 +1,7 @@
 Slack::Events.configure do |config|
-  config.signing_secret = Rails.application.credentials.slack.signing_secret
+  config.signing_secret = Rails.application.credentials.dig(:slack, :signing_secret)
 end
 
 Slack.configure do |config|
-  config.token = Rails.application.credentials.slack.bot_token
+  config.token = Rails.application.credentials.dig(:slack, :bot_token)
 end
