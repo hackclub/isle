e7cbf97d7afb02b000a0663a1a4a54e29812e08a
24c02 <163450896+24c02@users.noreply.github.com>
2025-08-26 15:49:16 -0400

    annotate models

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: scenes
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  thread_ts  :string
+#  x          :decimal(, )
+#  y          :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_scenes_on_user_id  (user_id)
+#
 class Scene < ApplicationRecord
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                                   :bigint           not null, primary key
+#  avatar                               :string
+#  display_name                         :string
+#  email                                :string
+#  first_name                           :string
+#  fraud_team_member                    :boolean          default(FALSE), not null
+#  freeze_shop_activity                 :boolean          default(FALSE)
+#  has_black_market                     :boolean
+#  has_clicked_completed_tutorial_modal :boolean          default(FALSE), not null
+#  has_commented                        :boolean          default(FALSE)
+#  has_hackatime                        :boolean          default(FALSE)
+#  has_hackatime_account                :boolean
+#  identity_vault_access_token          :string
+#  internal_notes                       :text
+#  is_admin                             :boolean          default(FALSE), not null
+#  is_banned                            :boolean          default(FALSE)
+#  last_name                            :string
+#  permissions                          :text             default("[]")
+#  shenanigans_state                    :jsonb
+#  synced_at                            :datetime
+#  timezone                             :string
+#  tutorial_video_seen                  :boolean          default(FALSE), not null
+#  ysws_verified                        :boolean          default(FALSE)
+#  created_at                           :datetime         not null
+#  updated_at                           :datetime         not null
+#  identity_vault_id                    :string
+#  slack_id                             :string
+#
 class User < SoMRecord
 
 end
