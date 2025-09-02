d1aacbc6389957b774028f2031dbc54cf80697e7
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-02 15:10:56 -0400

    fix manifest format

--- app/services/scenecore.rb
+++ app/services/scenecore.rb
@@ -2,7 +2,7 @@ module Scenecore
   class << self
     def import_from_json(text)
       data = JSON.parse(text, symbolize_names: true)
-      data[:scenes].each do |scene|
+      data.each do |scene|
         s = Scene.find_or_create_by(id: scene[:id])
         s.x, s.y = scene[:x], scene[:y]
         s.name = scene[:name]
