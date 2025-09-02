47342ca828e71666c0ea978dcea27e576d475d11
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-02 14:52:10 -0400

    allow framing!

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module Isle
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
