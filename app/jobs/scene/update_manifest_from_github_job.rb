a51e8fa32e813b7ee1d69f5a6dbe2487efb25930
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-04 12:20:03 -0400

    make the job job

--- app/jobs/scene/update_manifest_from_github_job.rb
+++ app/jobs/scene/update_manifest_from_github_job.rb
@@ -3,5 +3,6 @@ class Scene::UpdateManifestFromGitHubJob < ApplicationJob
 
   def perform(*args)
     data = Faraday.get("#{Rails.application.credentials.dig(:github, :pages_base_url)}manifest.json", headers: { user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36" }).body
+    Scenecore.import_from_json(data)
   end
 end
