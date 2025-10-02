0e7b340ca419cb783766e039e59a2c206183bb85
Euan R <rippereuan@gmail.com>
2025-10-01 20:47:50 -0400

    Add Cross-Origin headers to default headers
    Added Cross-Origin headers for security.

--- config/application.rb
+++ config/application.rb
@@ -38,9 +38,10 @@ module Isle
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
     config.action_dispatch.default_headers = {
-      "X-Frame-Options" => "ALLOWALL"
+      "X-Frame-Options" => "ALLOWALL",
+      "Cross-Origin-Embedder-Policy" => "require-corp",
+      "Cross-Origin-Opener-Policy"   => "same-origin"
     }
   end
 end
